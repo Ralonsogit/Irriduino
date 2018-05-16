8399baaab41080e6d1f0e84dc19da9a94b44ec53
Roberto Alonso <ralonsoalvarezr@gmail.com>
2018-05-16 04:57:45 +0200

    Update main (basic version)

--- main.ino
+++ main.ino
@@ -1,9 +1,12 @@
-// Add libraries
+// Libraries
 #include <Time.h>
 #include <TimeLib.h>
 
 // Set pin 4 for the relay
 int input_relay = 4;
+// Set start and stop hours (0-23) and minutes (0-59)
+byte start_hour = 1, start_minute = 11;
+byte stop_hour = 1, stop_minute = 12;
 
 // This function is going to be executed once
 void setup() {
@@ -11,13 +14,41 @@ void setup() {
     pinMode(input_relay, OUTPUT);
     // Swicht off the relay
     digitalWrite(input_relay, HIGH);
+    // Set time manually
+    setTime(1, 10, 00, 8, 5, 2018);
 }
 
 // This function is going to be executed as an infinite loop
 void loop() {
-    // Toggle relay signal waiting 3 secs between each one
-    digitalWrite(input_relay, LOW);
-    delay(3000);
-    digitalWrite(input_relay, HIGH);
-    delay(3000);
+    // Show actual time
+    Serial.print("It's ");
+    Serial.print(String(hour()) + ":" + String(minute()) + ":" + String(second()));
+
+    // Check if it is irrigation time
+    if (isIrrigationTime()) {
+        Serial.println("It's irrigation time !!");
+        digitalWrite (input_relay, HIGH);
+    }
+    else {
+        Serial.println("NOT irrigation time");
+        digitalWrite (input_relay, LOW);
+    }
+    delay(1000);
+}
+
+boolean isIrrigationTime() {
+    // Convert hours to minutes in order to compare
+    int start_time = (start_hour * 60) + start_minute;
+    int stop_time = (stop_hour * 60) + stop_minute;
+    int now_time = (hour() * 60) + minute();
+
+    // If time is between start and stop time... It's irrigation time !
+    if ((start_time <= now_time) && (now_time < stop_time)) {
+        // Irrigation time! =D
+        return true;
+    } 
+    else {
+        // Not irrigation time :(
+        return false;
+    }
 }
